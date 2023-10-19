a61d2dab0a42a76b6bfa3ace443fcfdf5486ee4c
Arthur Ataíde <arthursaraiva0704@gmail.com>
2023-10-19 17:45:09 -0300

    a

--- main.c
+++ main.c
@@ -14,56 +14,20 @@
 // }
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <termios.h>
-#include <unistd.h>
-#include <fcntl.h>
-
-int kbhit() {
-    struct termios oldt, newt;
-    int ch;
-    int oldf;
-
-    // Salva as configurações atuais do terminal
-    tcgetattr(STDIN_FILENO, &oldt);
-
-    // Desativa o modo canônico do terminal (leitura de linha por linha)
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-
-    // Configura o descritor de arquivo STDIN_FILENO para não bloquear
-    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-
-    ch = getchar();
-
-    // Restaura as configurações originais do terminal
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-
-    // Restaura as configurações do descritor de arquivo
-    fcntl(STDIN_FILENO, F_SETFL, oldf);
-
-    if (ch != EOF) {
-        ungetc(ch, stdin);
-        return 1;
-    }
-
-    return 0;
-}
+#include <conio.h>
 
 int main() {
     printf("Pressione qualquer tecla (ou 'q' para sair):\n");
 
+    int ch;
     while (1) {
-        if (kbhit()) {
-            int key = getchar();
-            if (key == 'q') {
-                break;  // Sai do loop se 'q' for pressionado
-            }
-            printf("Tecla pressionada: %c\n", key);
+        ch = _getch();  // Lê uma tecla pressionada
+        if (ch == 'q') {
+            break;  // Sai do loop se 'q' for pressionado
         }
+        printf("Tecla pressionada: %c\n", ch);
     }
 
     return 0;
 }
+
