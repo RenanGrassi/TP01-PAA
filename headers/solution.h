49b3a470c2c2c271ea748ede606e6363066d101d
Arthur-Ataide <arthursaraiva0704@gmail.com>
2023-10-24 15:46:52 -0300

    recursividade feita

--- headers/solution.h
+++ headers/solution.h
@@ -1,5 +1,11 @@
 #include "map.h"
 
+typedef struct TipoRecursividade{
+    int maiorRecursividade;
+    long int Recursividade;
+}TipoRecursividade;
+
+
 void mostraSequencia(int** routes, int tam); // mostra a sequencia da forma pedida no TP [0,0],[0,1]...
 
 bool checkingRoute(TipoMap *m, int* x, int* y, bool poder); // verifica se a rota eh valida, poder: true = pode passar pelas paredes, false = não pode passar pelas paredes
@@ -12,10 +18,14 @@ void reMovimentacao(TipoMap* map, int atualI, int atualJ); // volta a movimentac
 
 void irProxCaminho(PCaminho caminho, int tam, TipoMap* map, int** routes); // adiciona na lista encadeada de forma ordenada
 
-bool findShortestPath(int x, int y, int keys_collected, TipoMap* map, int** routes, int tam, int *caminhosJaVIstos); // achar o um caminho possivel recursivamente
+void mostraRecursividade(TipoRecursividade* rec); //  total de chamadas recursivas e o nível máximo de recursão
+
+void generateRec(TipoRecursividade* rec); // gera a recursividade
+
+bool findShortestPath(int x, int y, int keys_collected, TipoMap* map, int** routes, int tam, int *caminhosJaVIstos, int recursividade, TipoRecursividade* rec); // achar o um caminho possivel recursivamente
 
 void mostragemCaminho(TipoMap* map, PCaminho caminho, int* rotaN); // mostra todos os caminhos de forma recursiva
 
-void procurarCaminho(TipoMap* map, int* caminhosJaVistos);  // menu de mostragem de caminhos possiveis
+void procurarCaminho(TipoMap* map, int* caminhosJaVistos, TipoRecursividade* rec);  // menu de mostragem de caminhos possiveis
 
 
